d133c34b7335d8621310dba89c4eaaa1cb13c4c1
NFS12395-lmcdonald <blobfish2000@gmail.com>
2025-03-06 16:04:33 -0800

    MCTS works

--- stateful.py
+++ stateful.py
@@ -1,11 +1,13 @@
 import random
+from tqdm import tqdm
+from copy import deepcopy
 
 class State:
     values = range(13)
     val_chars = ["2","3","4","5","6","7","8","9","t","j","q","k","a"]
     val_pts = [2,3,4,5,6,7,8,9,10,10,10,10,11]
 
-    def __init__(self, decks, hands, plays, to_play, rd, done):
+    def __init__(self, decks = [[],[]], hands = [[],[]], plays = [[[],[],[]],[[],[],[]]], to_play=0, rd=0, done=[False,False]):
         self.decks = decks
         self.hands = hands
         self.plays = plays
@@ -13,18 +15,37 @@ class State:
         self.rd = rd
         self.done = done
 
+    def draw(self, n):
+        ##TODO THIS IS EXTREMELY WRONG
+        random.shuffle(self.decks[0])
+        random.shuffle(self.decks[1])
+
+        self.hands[0].append(self.decks[0][n])
+        self.hands[1].append(self.decks[1][n])
+
+
     def valid_moves(self):
-        if(self.done[to_play]):
+        if(self.done[self.to_play]):
             return [-1]
-        return [-1] + self.hands[self.to_play]
+        return [-1] + list(range(len(self.hands[self.to_play])))
 
 
-    def is_done(self):
-        return -3 in self.plays[0][2] and -3 in self.plays[1][2]
+    def game_over(self):
+        return self.rd > 2
 
-    def apply_move(self, move):
+    def eval_round(self, rd):
+        p1_round = self.plays[0][rd]
+        p2_round = self.plays[1][rd]
+        p1_scores = sum([State.val_pts[card] for card in p1_round])
+        p2_scores = sum([State.val_pts[card] for card in p2_round])
+        return 1 if p1_scores > p2_scores else (-1 if p2_scores > p1_scores else 0)
+
+    def eval_game(self):
+        return sum([self.eval_round(rd) for rd in range(self.rd)])
+
+    def after_move(self, move):
         if move == -1:
-            if self.done[0] and self.done[1]:
+            if self.done[[1,0][self.to_play]]:
                 return State(
                     self.decks,
                     self.hands,
@@ -39,10 +60,119 @@ class State:
                     self.plays,
                     [1,0][self.to_play],
                     self.rd,
-                    [self.done[0] or self.to_play == 0, self.done[1] or self.to_play == 1]
-                    )
+                    [self.done[0] or self.to_play == 0, self.done[1] or self.to_play == 1])
+        else:
+            plays = [[[card for card in rd] for rd in player] for player in self.plays]
+            plays[self.to_play][self.rd].append(self.hands[self.to_play][move])
+            hands = [[card for card in hand] for hand in self.hands]
+            hands[self.to_play] = self.hands[self.to_play][:move] + self.hands[self.to_play][move+1:]
+
+            return State(
+                    self.decks,
+                    hands,
+                    plays,
+                    [1,0][self.to_play],
+                    self.rd,
+                    self.done)
+
+    def visualize(self, show_p1 = True, show_p2 = True):
+
+        header = "player: " + str(self.to_play) + " round: " + str(self.rd)
+
+        p2_hand = " ".join("**" for card in self.hands[1])
+        if show_p2:
+            p2_hand = " ".join(State.val_chars[card] for card in self.hands[1])
+
+        p1_hand = " ".join("**" for card in self.hands[0])
+        if show_p1:
+            p1_hand = " ".join(State.val_chars[card] for card in self.hands[0])
+
+        p1_board = ""
+        p2_board = ""
+
+        for rd in range(min(self.rd+1, 3)):
+            max_cards = max(len(self.plays[0][rd]), len(self.plays[1][rd]))
+            p1_plays = [State.val_chars[cd] for cd in self.plays[0][rd]] + (max_cards - len(self.plays[0][rd])) * [" "]
+            p2_plays = [State.val_chars[cd] for cd in self.plays[1][rd]] + (max_cards - len(self.plays[1][rd])) * [" "]
+            p1_board += " ".join(p1_plays)
+            p2_board += " ".join(p2_plays)
+            if rd < self.rd:
+                p1_board += " | "
+                p2_board += " | "
+            else:
+                if self.done[0]:
+                    p1_board += " | "
+                if self.done[1]:
+                    p2_board += " | "
 
 
+        out = "\n".join([header, p2_hand, p2_board, p1_board, p1_hand])
+        return(out)
 
+class Node:
+    def __init__(self, move, parent, played):
+        self.move, self.parent, self.played, self.children = move, parent, played, []
+        self.wins, self.visits = 0, 0
+
+    def expand(self, state):
+        if not state.game_over():
+            for move in state.valid_moves():
+                node = Node(move, self, [1,0][state.to_play])
+                self.children.append(node)
+
+    def update(self, winner):
+        self.visits += 1
+        if winner > 0 and self.played == 1:
+            self.wins += 1
+        elif winner < 0 and self.played == 0:
+            self.wins += 1
+        elif winner == 0:
+            self.wins += 0.5
+
+def montesearch(state, i = 1000):
+    root = Node(None, None, [1,0][state.to_play])
+    for i in range(i):
+        n, s = root, deepcopy(state)
+        while not len(n.children) == 0:
+            n = random.choice(n.children) #do better
+            s = s.after_move(n.move)
+
+        n.expand(s)
+        if(len(n.children) == 0):
+            continue
+        n = random.choice(n.children)
         
+        while not s.game_over():
+            s = s.after_move(random.choice(s.valid_moves()))
 
+        r = s.eval_game()
+        while not n.parent == None:
+            n.update(r)
+            n = n.parent
+
+    return list(filter(lambda n: n.wins == max([node.wins for node in root.children]), root.children))[0].move
+
+
+
+#winrates
+player1 = 0
+player2 = 0
+for i in tqdm(range(1,1001)):
+    state = State([list(range(3,10)),list(range(3,10))], [[],[]], [[[],[],[]],[[],[],[]]], 0, 0, [False, False])
+    state = state.draw(5)
+    while not state.game_over():
+        #print("-"*20)
+        move = montesearch(state)
+        #print("player", str(state.to_play) + " playing " + (["skip"] + [State.val_chars[card] for card in state.hands[state.to_play]])[move + 1])
+        state = state.after_move(move)
+        #print(state.visualize())
+    winner = state.eval_game()
+    if winner > 0:
+        player1 += 1
+    if winner < 0:
+        player2 += 1
+
+    print("player1:" + str(player1/i))
+    print("player2:" + str(player2/i))
+
+print(state.eval_game())
